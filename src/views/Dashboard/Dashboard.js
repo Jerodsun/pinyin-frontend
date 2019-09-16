0b812b686a63347f08b4533927f264cd3dc3c4b5
Jerod Sun <Jerod.sun@live.com>
2019-09-15 20:00:59 -0400

    working axios local componentDidMount()

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'; // lazy, Suspense 
+import { Button, Card, CardBody, CardHeader, Col, Row } from 'reactstrap';
 // import { Bar, Line } from 'react-chartjs-2';
 // import {
 //   Badge,
@@ -22,60 +23,120 @@ import React, { Component } from 'react'; // lazy, Suspense
 // } from 'reactstrap';
 // import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities'
 
-// Main Chart
+const axios = require('axios').default;
 
-//Random Numbers
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-var elements = 27;
-var data1 = [];
-var data2 = [];
-var data3 = [];
-
-for (var i = 0; i <= elements; i++) {
-  data1.push(random(50, 200));
-  data2.push(random(80, 100));
-  data3.push(65);
-}
+const API = 'http://127.0.0.1:8000/api/pinyin/'
 
 
-class Dashboard extends Component {
+
+export default class Dashboard extends Component {
   constructor(props) {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
-
     this.state = {
-      dropdownOpen: false,
-      radioSelected: 2,
-    };
+      apiCall:false,
+      apiResult: [],
+    }
   }
 
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen,
-    });
-  }
+  componentDidMount() {
 
-  onRadioBtnClick(radioSelected) {
-    this.setState({
-      radioSelected: radioSelected,
-    });
-  }
+    // axios.get(API)
+    //     .then(response => {
+    //       console.log(response);
+    //     })
+    //     .catch(error => {
+    //       console.log(error);
+    //     });
 
+    axios.post(API, {
+      "input_string": "孫子曰：兵者，國之大事，死生之地，存亡之道，不可不察也。"
+        }
+    )
+        .then(response => {
+          if (response.data.message === "success") {
+            this.setState( { apiResult : response.data} );
+            console.log(this.state.apiResult )
+            console.log(response)
+          }
+          
+          else {
+            console.log("Error!")
+            console.log(response)
+          }
+          
+          
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+    // axios.post(API, {
+    //   "input_string": "孫子曰：兵者，國之大事，死生之地，存亡之道，不可不察也。"
+    //     }
+    // )
+    //     .then(response => response.data)
+    //     .then((data) => {
+    //       this.setState( { apiResult : data } );
+    //       console.log( this.state.apiResult )
+
+    //     })
+  }
+  
   loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>
 
   render() {
 
     return (
       <div className="animated fadeIn">
+        <Card>
+          <CardHeader>
+            <strong>Standard Buttons</strong>
+          </CardHeader>
+          <CardBody>
+            <Row className="align-items-center">
+              <Col col="12" xl className="mb-3 mb-xl-0">
+                Normal
+              </Col>
+              <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
+                <Button block color="primary">Primary</Button>
+              </Col>
+              <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
+                <Button block color="secondary">Secondary</Button>
+              </Col>
+              <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
+                <Button block color="success">Success</Button>
+              </Col>
 
+            </Row>
+          </CardBody>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <strong>Standard Buttons</strong>
+          </CardHeader>
+          <CardBody>
+            <Row className="align-items-center">
+              <Col col="12" xl className="mb-3 mb-xl-0">
+                Normal
+              </Col>
+              <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
+                <Button block color="primary">Primary</Button>
+              </Col>
+              <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
+                <Button block color="secondary">Secondary</Button>
+              </Col>
+              <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
+                <Button block color="success">Success</Button>
+              </Col>
+
+            </Row>
+          </CardBody>
+        </Card>
+ 
       </div>
+
     );
   }
 }
-
-export default Dashboard;

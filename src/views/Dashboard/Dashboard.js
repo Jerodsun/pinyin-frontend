05809e319f231d1dcbc2f7160c37a33d3e26fe8f
Jerod Sun <Jerod.sun@live.com>
2019-09-21 17:05:32 -0400

    es6 style react

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'; // lazy, Suspense 
 import {
-  Badge,
   Button,
   Card,
   CardBody,
@@ -32,36 +31,56 @@ export default class Dashboard extends Component {
   }
 
   componentDidMount() {
+    this.axiosPost();
+  }
 
-    // axios.get(API)
-    //     .then(response => {
-    //       console.log(response);
-    //     })
-    //     .catch(error => {
-    //       console.log(error);
-    //     });
-
-    axios.post(API, {
-      "input_string": "孫子曰：兵者，國之大事，死生之地，存亡之道，不可不察也。"
+  axiosPost = () => {
+    axios
+      .post(API, {
+        "input_string": "孫子曰：兵者，國之大事，死生之地，存亡之道，不可不察也。"
+          }
+      )
+      .then(response => {
+        if (response.data.message === "success") {
+          this.setState( { apiResult : response.data} );
+          console.log(this.state.apiResult )
+          console.log(response)
         }
-    )
-        .then(response => {
-          if (response.data.message === "success") {
-            this.setState( { apiResult : response.data} );
-            console.log(this.state.apiResult )
-            console.log(response)
-          }
-          
-          else {
-            console.log("Error!")
-            console.log(response)
-          }
-          
-          
-        })
-        .catch(error => {
-          console.log(error);
-        });
+        else {
+          console.log("Error!")
+          console.log(response)
+        }  
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  buttonsCard = () => {
+    return (
+      <Card>
+        <CardHeader>
+          <strong>Standard Buttons</strong>
+        </CardHeader>
+        <CardBody>
+          <Row className="align-items-center">
+            <Col col="12" xl className="mb-3 mb-xl-0">
+              Normal
+            </Col>
+            <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
+              <Button block color="primary">Primary</Button>
+            </Col>
+            <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
+              <Button block color="secondary">Secondary</Button>
+            </Col>
+            <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
+              <Button block color="success">Success</Button>
+            </Col>
+
+          </Row>
+        </CardBody>
+      </Card>
+    );
   }
   
   loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>
@@ -70,28 +89,7 @@ export default class Dashboard extends Component {
 
     return (
       <div className="animated fadeIn">
-        <Card>
-          <CardHeader>
-            <strong>Standard Buttons</strong>
-          </CardHeader>
-          <CardBody>
-            <Row className="align-items-center">
-              <Col col="12" xl className="mb-3 mb-xl-0">
-                Normal
-              </Col>
-              <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
-                <Button block color="primary">Primary</Button>
-              </Col>
-              <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
-                <Button block color="secondary">Secondary</Button>
-              </Col>
-              <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
-                <Button block color="success">Success</Button>
-              </Col>
-
-            </Row>
-          </CardBody>
-        </Card>
+        {this.buttonsCard()}
 
         <Card>
               <CardHeader>
@@ -101,64 +99,14 @@ export default class Dashboard extends Component {
                 <Form action="" method="post" encType="multipart/form-data" className="form-horizontal">
                   <FormGroup row>
                     <Col md="3">
-                      <Label>Static</Label>
-                    </Col>
-                    <Col xs="12" md="9">
-                      <p className="form-control-static">Username</p>
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
                       <Label htmlFor="text-input">Text Input</Label>
                     </Col>
                     <Col xs="12" md="9">
-                      <Input type="text" id="text-input" name="text-input" placeholder="Text" />
+                      <Input type="textarea" id="text-input" name="text-input" placeholder="Text" />
                       <FormText color="muted">This is a help text</FormText>
                     </Col>
                   </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="email-input">Email Input</Label>
-                    </Col>
-                    <Col xs="12" md="9">
-                      <Input type="email" id="email-input" name="email-input" placeholder="Enter Email" autoComplete="email"/>
-                      <FormText className="help-block">Please enter your email</FormText>
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="password-input">Password</Label>
-                    </Col>
-                    <Col xs="12" md="9">
-                      <Input type="password" id="password-input" name="password-input" placeholder="Password" autoComplete="new-password" />
-                      <FormText className="help-block">Please enter a complex password</FormText>
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="date-input">Date Input <Badge>NEW</Badge></Label>
-                    </Col>
-                    <Col xs="12" md="9">
-                      <Input type="date" id="date-input" name="date-input" placeholder="date" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="disabled-input">Disabled Input</Label>
-                    </Col>
-                    <Col xs="12" md="9">
-                      <Input type="text" id="disabled-input" name="disabled-input" placeholder="Disabled" disabled />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="textarea-input">Textarea</Label>
-                    </Col>
-                    <Col xs="12" md="9">
-                      <Input type="textarea" name="textarea-input" id="textarea-input" rows="9"
-                             placeholder="Content..." />
-                    </Col>
-                  </FormGroup>
+                  
                   <FormGroup row>
                     <Col md="3">
                       <Label htmlFor="select">Select</Label>
@@ -172,52 +120,11 @@ export default class Dashboard extends Component {
                       </Input>
                     </Col>
                   </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="selectLg">Select Large</Label>
-                    </Col>
-                    <Col xs="12" md="9" size="lg">
-                      <Input type="select" name="selectLg" id="selectLg" bsSize="lg">
-                        <option value="0">Please select</option>
-                        <option value="1">Option #1</option>
-                        <option value="2">Option #2</option>
-                        <option value="3">Option #3</option>
-                      </Input>
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="selectSm">Select Small</Label>
-                    </Col>
-                    <Col xs="12" md="9">
-                      <Input type="select" name="selectSm" id="SelectLm" bsSize="sm">
-                        <option value="0">Please select</option>
-                        <option value="1">Option #1</option>
-                        <option value="2">Option #2</option>
-                        <option value="3">Option #3</option>
-                        <option value="4">Option #4</option>
-                        <option value="5">Option #5</option>
-                      </Input>
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="disabledSelect">Disabled Select</Label>
-                    </Col>
-                    <Col xs="12" md="9">
-                      <Input type="select" name="disabledSelect" id="disabledSelect" disabled autoComplete="name">
-                        <option value="0">Please select</option>
-                        <option value="1">Option #1</option>
-                        <option value="2">Option #2</option>
-                        <option value="3">Option #3</option>
-                      </Input>
-                    </Col>
-                  </FormGroup>
                 </Form>
               </CardBody>
               <CardFooter>
                 <Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>
-                <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
+                {/* <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button> */}
               </CardFooter>
             </Card>
        
